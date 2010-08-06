64a98a2f99f86bcba00c9f6e376bdec2719409a4
Nick Piper <nick.piper@cgi.com>
2010-08-06 14:21:02 +0000

    Improve our customisation of the ReportModule, which doesn't offer the list of Reports (as we have the drop down)
    See #9
    
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@958 484df14f-75d2-4b3f-8ba0-490e05069a01

--- userqueries/report.py
+++ userqueries/report.py
@@ -1,11 +1,51 @@
 from trac.ticket.report import ReportModule
+from trac.web.chrome import add_stylesheet, add_ctxtnav, Chrome
+from trac.util.translation import _
 
 class LogicaReportModule(ReportModule):
     def process_request(self, req):
         # We don't want to show the user the list that ReportModule implements.
         # We have a better navigation system.
-        if not req.args.has_key('id'):
-            # TODO: load out of the user preferences
-            req.args['id'] = 1
-        return ReportModule.process_request(self, req)
 
+        req.perm.require('REPORT_VIEW')
+
+        # did the user ask for any special report?
+        id = int(req.args.get('id', 1)) # TODO: we should get the default one out of their preferences?
+        action = req.args.get('action', 'view')
+
+        data = {}
+        if req.method == 'POST':
+            if action == 'new':
+                self._do_create(req)
+            elif action == 'delete':
+                self._do_delete(req, id)
+            elif action == 'edit':
+                self._do_save(req, id)
+        elif action in ('copy', 'edit', 'new'):
+            template = 'report_edit.html'
+            data = self._render_editor(req, id, action=='copy')
+            Chrome(self.env).add_wiki_toolbars(req)
+        elif action == 'delete':
+            template = 'report_delete.html'
+            data = self._render_confirm_delete(req, id)
+        elif id == -1:
+            template, data, content_type = self._render_list(req)
+            if content_type: # i.e. alternate format
+                return template, data, content_type
+        else:
+            template, data, content_type = self._render_view(req, id)
+            if content_type: # i.e. alternate format
+                return template, data, content_type
+
+        # Kludge: only show link to custom query if the query module is actually
+        # enabled
+        from trac.ticket.query import QueryModule
+        if 'TICKET_VIEW' in req.perm and \
+                self.env.is_component_enabled(QueryModule):
+            add_ctxtnav(req, _('Custom Query'), href=req.href.query())
+            data['query_href'] = req.href.query()
+        else:
+            data['query_href'] = None
+
+        add_stylesheet(req, 'common/css/report.css')
+        return template, data, None
